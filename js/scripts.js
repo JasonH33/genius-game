70472a295227c7b5667a9f93f2d39bf409ad5bdc
JasonH33 <Jason.a.a.lourenco@outlook.com>
2021-08-15 19:35:27 -0300

    start, score, colors blink

--- js/scripts.js
+++ js/scripts.js
@@ -49,7 +49,9 @@ _gui.strict.addEventListener('click', () => {
   _data.strict = _gui.led.classList.toggle('gui__led--active')
 })
 
-_gui.start.addEventListener('click', () => {})
+_gui.start.addEventListener('click', () => {
+  startGame()
+})
 
 const padListener = e => {}
 
@@ -57,15 +59,48 @@ _gui.pads.forEach(pad => {
   pad.addEventListener('click', padListener)
 })
 
-const startGame = () => {}
+const startGame = () => {
+  blink('--', () => {
+    newColor()
+  })
+}
 
-const setScore = () => {}
+const setScore = () => {
+  const score = _data.score.toString()
+  const display = '00'.substring(0, 2 - score.lenght) + score
+  _gui.counter.innerHTML = display
+}
 
-const newColor = () => {}
+const newColor = () => {
+  _data.gameSequence.push(Math.floor(Math.random() * 4))
+  _data.score++
+  setScore()
+}
 
 const playSequence = () => {}
 
-const blink = (text, callback) => {}
+const blink = (text, callback) => {
+  let counter = 0,
+    on = true
+  _gui.counter.innerHTML = text
+  const interval = setInterval(() => {
+    if (!_data.gameOn) {
+      clearInterval(interval)
+      _gui.counter.classList.remove('gui__counter--on')
+      return
+    }
+    if (on) {
+      _gui.counter.classList.remove('gui__counter--on')
+    } else {
+      _gui.counter.classList.add('gui__counter--on')
+      if (++counter === 3) {
+        clearInterval(interval)
+        callback()
+      }
+    }
+    on = !on
+  }, 250)
+}
 
 const waitForPlayerClick = () => {}
 
